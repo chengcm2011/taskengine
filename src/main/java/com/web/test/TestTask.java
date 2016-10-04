277301d5a4dd6206868b3e2baedde1082bdddef3
cheng <chengcm2011@163.com>
2016-10-04 15:07:50 +0800

    去掉parent

--- src/main/java/com/web/test/TestTask.java
+++ src/main/java/com/web/test/TestTask.java
@@ -4,8 +4,6 @@ import cheng.lib.exception.BusinessException;
 import cheng.lib.util.Predef;
 import com.application.taskengine.AbstractTaskImpl;
 import org.quartz.JobDataMap;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Created by cheng on 2015/10/23.
@@ -16,7 +14,7 @@ public class TestTask extends AbstractTaskImpl {
 	public String doexecute(JobDataMap jobDataMap) throws BusinessException {
 		Predef.p(jobDataMap);
 		try {
-			Thread.sleep(30000);
+			Thread.sleep(300000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
