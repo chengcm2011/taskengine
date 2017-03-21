7a507ffdf7cb9c6c160d3810a7e7d611e24f51a7
cheng <chengcm2011@163.com>
2017-03-21 23:31:17 +0800

    删除无用代码

--- src/main/java/com/web/test/TestTask.java
+++ src/main/java/com/web/test/TestTask.java
@@ -1,6 +1,5 @@
 package com.web.test;
 
-import com.application.conf.core.ConfClient;
 import com.application.taskengine.AbstractTaskImpl;
 import com.cheng.lang.exception.BusinessException;
 import com.cheng.util.Predef;
@@ -13,8 +12,8 @@ public class TestTask extends AbstractTaskImpl {
 
     @Override
     public String doexecute(JobDataMap jobDataMap) throws BusinessException {
-//		Predef.p(jobDataMap);
-        Predef.p("get conf sdsdsdsd: " + ConfClient.getInstance().get("sdsdsdsd", "没有获取到配置"));
+        Predef.p(jobDataMap);
+//        Predef.p("get conf sdsdsdsd: " + ConfClient.getInstance().get("sdsdsdsd", "没有获取到配置"));
         return SUCCESS;
     }
 }
