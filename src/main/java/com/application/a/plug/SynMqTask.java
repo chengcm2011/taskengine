e0a59cf732e5242dfa509fd0da9685d3fed3adb8
chengys4 <chengys4@ziroom.com>
2017-04-10 18:25:11 +0800

    同步户号

--- src/main/java/com/application/a/plug/SynMqTask.java
+++ src/main/java/com/application/a/plug/SynMqTask.java
@@ -3,7 +3,7 @@ package com.application.a.plug;
 import com.application.a.itf.ISynService;
 import com.application.a.model.AccountNOModel;
 import com.application.taskengine.AbstractTaskImpl;
-import com.cheng.jdbc.itf.IBaseDAO;
+import com.cheng.jdbcspring.IDataBaseService;
 import com.cheng.lang.PageVO;
 import com.cheng.lang.exception.BusinessException;
 import com.cheng.util.Predef;
@@ -24,7 +24,7 @@ public class SynMqTask extends AbstractTaskImpl {
         PageVO pageVO = new PageVO();
         pageVO.setPageSize(Predef.toInt(jobDataMap.getIntValue("pageSize"), 1));
         pageVO.setCondition(" success='N' ");
-        ApplicationServiceLocator.getBean(IBaseDAO.class).queryByPage(AccountNOModel.class, pageVO);
+        ApplicationServiceLocator.getBean(IDataBaseService.class).queryByPage(AccountNOModel.class, pageVO);
         List<AccountNOModel> data = (List<AccountNOModel>) pageVO.getData();
         for (AccountNOModel accountNOModel : data) {
             synService.syn(accountNOModel);
